102dce6ab9d296d869205e5ed5cd6f7de55caa4e
mastaki <mastakiradjabo631@gmail.com>
2026-01-21 07:45:08 +0200

    stylisation du bouton choisir une photo Bouton.tsx, boutonViewer.js et index.js ajout du props theme au bouton en question

--- composants/Bouton.tsx
+++ composants/Bouton.tsx
@@ -1,11 +1,31 @@
 import { boutonViewer } from "@/styles/composants/boutonViewer";
+import React from "react";
 import { Pressable, View, Text } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
 
 type Props = {
     label: string;
+    theme?: 'primary';
 };
 
-export default function Bouton({ label }: Props) {
+export default function Bouton({ label, theme }: Props) {
+    if (theme === 'primary') {
+        return (
+            <View style={[boutonViewer.boutonContainer, 
+                {borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18,}
+            ]}>
+                <Pressable 
+                    style={[boutonViewer.bouton, 
+                        {backgroundColor: '#fff',}
+                    ]} 
+                    onPress={() => alert('Vous avez presse un bouton.')}
+                >
+                    <FontAwesome name="picture-o" size={18} color="#25292e" style={boutonViewer.boutonIcon} />
+                    <Text style={[boutonViewer.boutonLabel, {color: '#25292e',}]}>{label}</Text>
+                </Pressable>
+            </View>
+        )
+    }
     return (
         <View style={boutonViewer.boutonContainer}>
             <Pressable style={boutonViewer.bouton} onPress={() => alert('Vous avez presse un bouton.')}>
